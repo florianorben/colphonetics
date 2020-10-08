1c146f1d63e2e8729da9632960a518864e84bc98
Lorenz Blechschmidt <lorenz.blechschmidt@schmetterling.de>
2020-10-08 12:40:38 +0200

    because colphonetics is case insensitive, we can simply convert string to lower case and remove all case sensitive rating mappings

--- colphonetics.go
+++ colphonetics.go
@@ -6,26 +6,10 @@ import "strings"
 
 var (
 	replacer = strings.NewReplacer(
-		"Ä", "a",
-		"Ö", "o",
-		"Ü", "u",
 		"ä", "a",
 		"ö", "o",
 		"ü", "u",
-		"PH", "F",
-		"Ph", "F",
-		"pH", "f",
 		"ph", "f",
-		"Ç", "c",
-		"É", "e",
-		"È", "e",
-		"Ê", "e",
-		"À", "A",
-		"Á", "a",
-		"Â", "a",
-		"Ë", "e",
-		"Í", "i",
-		"Ì", "i",
 		"ç", "c",
 		"é", "e",
 		"è", "e",
@@ -40,32 +24,6 @@ var (
 	)
 
 	charToScore = map[int32]string{
-		65:  "0",  //A
-		66:  "1",  //B
-		67:  "4",  //C
-		68:  "2",  //D
-		69:  "0",  //E
-		70:  "3",  //F
-		71:  "4",  //G
-		72:  "",   //H
-		73:  "0",  //I
-		74:  "0",  //J
-		75:  "4",  //K
-		76:  "5",  //L
-		77:  "6",  //M
-		78:  "6",  //N
-		79:  "0",  //O
-		80:  "1",  //P
-		81:  "4",  //Q
-		82:  "7",  //R
-		83:  "8",  //S
-		84:  "2",  //T
-		85:  "0",  //U
-		86:  "3",  //V
-		87:  "3",  //W
-		88:  "48", //X
-		89:  "0",  //Y
-		90:  "8",  //Z
 		97:  "0",  //a
 		98:  "1",  //b
 		99:  "4",  //c
@@ -95,11 +53,6 @@ var (
 	}
 
 	exceptionsFollowing = map[string]string{
-		"SC": "8",
-		"ZC": "8",
-		"CX": "8",
-		"KX": "8",
-		"QX": "8",
 		"sc": "8",
 		"zc": "8",
 		"cx": "8",
@@ -108,14 +61,6 @@ var (
 	}
 
 	exceptionsLeading = map[string]string{
-		"CA": "4",
-		"CH": "4",
-		"CK": "4",
-		"CL": "4",
-		"CO": "4",
-		"CQ": "4",
-		"CU": "4",
-		"CX": "4",
 		"ca": "4",
 		"ch": "4",
 		"ck": "4",
@@ -124,12 +69,6 @@ var (
 		"cq": "4",
 		"cu": "4",
 		"cx": "4",
-		"DC": "8",
-		"DS": "8",
-		"DZ": "8",
-		"TC": "8",
-		"TS": "8",
-		"TZ": "8",
 		"dc": "8",
 		"ds": "8",
 		"dz": "8",
@@ -141,12 +80,14 @@ var (
 
 // Code returns the phonetic code for a given string.
 func Code(s string) string {
+	s = strings.ToLower(s)
+
 	s = replacer.Replace(s)
 	l := len(s)
 	r := make([]string, l, l)
 
 	for i, char := range s {
-		if i == 0 && len(s) > 1 && (char == 67 && s[i+1] == 83 || char == 98 && s[i+1] == 115) {
+		if i == 0 && len(s) > 1 && char == 98 && s[i+1] == 115 {
 			r[i] = "4"
 		}
 
